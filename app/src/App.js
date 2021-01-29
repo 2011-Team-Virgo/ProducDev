37d87d4284876dbf171df39a00047c9b7361f402
Kerri Ryan <kerri.ryan11@gmail.com>
2021-01-29 14:56:48 -0500

    feat: FE: sign in, home, navbar, and routes to analytics and account pages

--- app/src/App.js
+++ app/src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import Home from './Home';
+import NavBar from './NavBar';
+import Analytics from './Analytics';
+import Account from './Account';
+import Signin from './Signin';
+import { Route, Switch } from 'react-router-dom';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles({});
 
 function App() {
+  const classes = useStyles();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={classes.container}>
+      <NavBar />
+      <Switch>
+        <Route exact path="/" render={(props) => <Home {...props} />} />
+        <Route
+          exact
+          path="/analytics"
+          render={(props) => <Analytics {...props} />}
+        />
+        <Route
+          exact
+          path="/account"
+          render={(props) => <Account {...props} />}
+        />
+        <Route exact path="/signin" render={(props) => <Signin {...props} />} />
+      </Switch>
     </div>
   );
 }
