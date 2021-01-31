509e3468afc8f9cbf0b9bb4d84838e4591a84d39
Kerri Ryan <kerri.ryan11@gmail.com>
2021-01-31 17:34:21 -0500

    feat: google auth new user created in db

--- app/src/index.js
+++ app/src/index.js
@@ -5,6 +5,18 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import history from './history';
 import { Router } from 'react-router-dom';
+import firebase from 'firebase/app';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyAe9TREuTK9UgMRKcibsnRPZnH7VRIQqZE',
+  authDomain: 'producdev-1277b.firebaseapp.com',
+  databaseURL: 'https://producdev-1277b-default-rtdb.firebaseio.com',
+  projectId: 'producdev-1277b',
+  storageBucket: 'producdev-1277b.appspot.com',
+  messagingSenderId: '908605484955',
+  appId: '1:908605484955:web:9cd8ccead875a6d40a3b3e',
+  measurementId: 'G-WG6Z0ZHZVM',
+};
 
 ReactDOM.render(
   <Router history={history}>
@@ -17,3 +29,4 @@ ReactDOM.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+firebase.initializeApp(firebaseConfig);
