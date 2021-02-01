d989b4de7e64a40b86f2424b40157b0e9aaba275
Kerri Ryan <kerri.ryan11@gmail.com>
2021-02-01 13:22:44 -0500

    fix: hid firebase config

--- app/src/firebase/index.js
+++
@@ -1,9 +0,0 @@
-import firebase from 'firebase/app';
-import "firebase/auth";
-import "firebase/database";
-import {firebaseConfig} from "./firebase_config.js"
-
-console.log("Initializing firebase")
-firebase.initializeApp(firebaseConfig)
-
-export default firebase;
--- app/src/index.js
+++ app/src/index.js
@@ -6,17 +6,9 @@ import reportWebVitals from './reportWebVitals';
 import history from './history';
 import { Router } from 'react-router-dom';
 import firebase from 'firebase/app';
-
-const firebaseConfig = {
-  apiKey: 'AIzaSyAe9TREuTK9UgMRKcibsnRPZnH7VRIQqZE',
-  authDomain: 'producdev-1277b.firebaseapp.com',
-  databaseURL: 'https://producdev-1277b-default-rtdb.firebaseio.com',
-  projectId: 'producdev-1277b',
-  storageBucket: 'producdev-1277b.appspot.com',
-  messagingSenderId: '908605484955',
-  appId: '1:908605484955:web:9cd8ccead875a6d40a3b3e',
-  measurementId: 'G-WG6Z0ZHZVM',
-};
+import firebaseConfig from './firebase/firebase_config';
+import 'firebase/auth';
+import 'firebase/database';
 
 ReactDOM.render(
   <Router history={history}>
