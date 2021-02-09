ff4807671f9ab8f9da51a8c6b9053fe2bc4d7159
AnthonyMCorreia <anthonycorreia12@gmail.com>
2021-02-09 12:54:55 -0800

    first elements of the landing page

--- app/src/Analytics.js
+++ app/src/Analytics.js
@@ -1,30 +1,25 @@
-import React, { useState, useEffect } from 'react';
-import {useSelector} from 'react-redux'
-import { withRouter } from 'react-router-dom';
-import firebase from 'firebase/app';
+import React, { useState, useEffect } from "react";
+import { useSelector } from "react-redux";
+import { withRouter } from "react-router-dom";
+import firebase from "firebase/app";
+import * as d3 from "d3";
 
-const Analytics = () =>{
+const Analytics = () => {
   // const user = useSelector(state=>state.user)
 
-  const [data,setData] = useState({})
-  useEffect(()=>{
+  const [data, setData] = useState({});
+  useEffect(() => {
     var userId = firebase.auth().currentUser.id;
     firebase
-    .database()
-    .ref('users/' + userId)
-    .once('value')
-    .then((snapshot) => {
-      setData(snapshot.val())
-    });
-  },[])
-  
-  console.log(data)
-  
+      .database()
+      .ref("users/" + userId)
+      .once("value")
+      .then((snapshot) => {
+        setData(snapshot.val());
+      });
+  }, []);
 
-return(
-  <h1>Hi {data.name}</h1>
-  
-) 
-}
+  return <svg height="80" width="95"></svg>;
+};
 
 export default Analytics;
