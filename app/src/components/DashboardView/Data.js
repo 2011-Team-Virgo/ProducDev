3cc74f15eecb0de55d36c3429754e8378bee705a
Kerri Ryan <kerri.ryan11@gmail.com>
2021-02-15 17:25:16 -0500

    feat: added summary paper to analytics page

--- app/src/components/DashboardView/Data.js
+++ app/src/components/DashboardView/Data.js
@@ -8,7 +8,9 @@ import {
   Select,
   NativeSelect,
   Container,
-  MenuItem
+  MenuItem,
+  Grid,
+  Paper,
 } from "@material-ui/core";
 import { useSelector, useDispatch } from "react-redux";
 import { fetchUserData } from "../../store/user.js";
@@ -27,14 +29,22 @@ const useStyles = makeStyles(() => ({
   root: {},
   formControl: {
     backgroundColor: "#FFFFFF",
-    margin:"1%",
+    margin: "1%",
     minWidth: 120,
   },
-  lineChart:{
-    display:"flex",
-    justifyContent:"center",
-    margin:"5%"
-  }
+  lineChart: {
+    display: "flex",
+    justifyContent: "center",
+    margin: "5%",
+  },
+  paper: {
+    backgroundColor: "transparent",
+    color: "#FEFFFF",
+    "&:hover": {
+      opacity: "0.6",
+    },
+    border: [[1, "solid", "#2ec4b6"]],
+  },
 }));
 
 const Data = (props) => {
@@ -78,28 +88,28 @@ const Data = (props) => {
     return result;
   };
   const projectData = projects && cleanData(projects);
-  
+  console.log(projectData);
+
   //State
   const [state, setState] = React.useState({
     selectedProject: "",
     selectedFile: "",
-    projects:projectData || [],
-    files:[],
-    data:[]
+    projects: projectData || [],
+    files: [],
+    data: [],
   });
 
   const projHandleChange = (event) => {
-
     const name = event.target.name;
     const value = event.target.value;
 
-    if(value === ""){
+    if (value === "") {
       setState({
-        [name]:value,
+        [name]: value,
         selectedFile: "",
-        data:[]
-      })
-    }else{
+        data: [],
+      });
+    } else {
       setState({
         ...state,
         [name]: value,
@@ -108,28 +118,184 @@ const Data = (props) => {
   };
 
   const fileHandleChange = (event) => {
-   
     const name = event.target.name;
     const value = event.target.value;
-    const project = state.selectedProject
+    const project = state.selectedProject;
     const data = projectData
-    .filter((project)=>state.selectedProject === project.name)[0]
-    .fileData
-    .filter((file)=>value === file.fileName)[0]
-    .data
+      .filter((project) => state.selectedProject === project.name)[0]
+      .fileData.filter((file) => value === file.fileName)[0].data;
 
-    if(value === ""){
+    if (value === "") {
       setState({
-        [name]:value,
-        data:[]
-      })
-    }else{
+        [name]: value,
+        data: [],
+      });
+    } else {
       setState({
         ...state,
         [name]: value,
-        data:data
+        data: data,
       });
     }
+  };
+
+  //if no project is selected, renders all data:
+  const allProjects = (arr) => {
+    let allProjects = [];
+    arr.map((elem) => {
+      return elem.fileData.map((e) => {
+        return e.data.map((x) => {
+          return allProjects.push(x);
+        });
+      });
+    });
+    return allProjects;
+  };
+
+  const allTime = () => {
+    let keys = 0;
+    projectData && allProjects(projectData).map((x) => (keys += x.keyStrokes));
+
+    let minutes = 0;
+    projectData && allProjects(projectData).map((x) => (minutes += x.minutes));
+
+    return `Since using ProducDev, you have made ${keys} keystrokes and coded for ${minutes} minutes`;
+  };
+
+  const dayOfWeek = () => {
+    let days = {
+      Monday: 0,
+      Tuesday: 1,
+      Wednesday: 2,
+      Thursday: 3,
+      Friday: 4,
+      Saturday: 5,
+      Sunday: 6,
+    };
+    let monMins = 0;
+    let monday =
+      projectData &&
+      allProjects(projectData)
+        .filter((x) => new Date(x.date).getDay() === 0)
+        .map((x) => (monMins += x.minutes));
+    days.Monday = monMins;
+
+    let tuesMins = 0;
+    let tuesday =
+      projectData &&
+      allProjects(projectData)
+        .filter((x) => new Date(x.date).getDay() === 1)
+        .map((x) => (tuesMins += x.minutes));
+    days.Tuesday = tuesMins;
+
+    let wedMins = 0;
+    let wednesday =
+      projectData &&
+      allProjects(projectData)
+        .filter((x) => new Date(x.date).getDay() === 2)
+        .map((x) => (wedMins += x.minutes));
+    days.Wednesday = wedMins;
+
+    let thursMins = 0;
+    let thursday =
+      projectData &&
+      allProjects(projectData)
+        .filter((x) => new Date(x.date).getDay() === 3)
+        .map((x) => (thursMins += x.minutes));
+    days.Thursday = thursMins;
+
+    let friMins = 0;
+    let friday =
+      projectData &&
+      allProjects(projectData)
+        .filter((x) => new Date(x.date).getDay() === 4)
+        .map((x) => (friMins += x.minutes));
+    days.Friday = friMins;
+
+    let satMins = 0;
+    let saturday =
+      projectData &&
+      allProjects(projectData)
+        .filter((x) => new Date(x.date).getDay() === 5)
+        .map((x) => (satMins += x.minutes));
+    days.Saturday = satMins;
+
+    let sunMins = 0;
+    let sunday =
+      projectData &&
+      allProjects(projectData)
+        .filter((x) => new Date(x.date).getDay() === 6)
+        .map((x) => (sunMins += x.minutes));
+    days.Sunday = sunMins;
+
+    let maxDay = Object.keys(days).reduce((a, b) =>
+      days[a] > days[b] ? a : b
+    );
+    return `Your most productive day of the week is ${maxDay}`;
+  };
+
+  const thirtyDays = () => {
+    let thirtyDays = new Date();
+    thirtyDays.setDate(thirtyDays.getDate() - 30);
+
+    let keys = 0;
+    let minutes = 0;
+
+    projectData &&
+      allProjects(projectData)
+        .filter((x) => new Date(x.date) >= thirtyDays)
+        .map((x) => (keys += x.keyStrokes));
+
+    projectData &&
+      allProjects(projectData)
+        .filter((x) => new Date(x.date) >= thirtyDays)
+        .map((x) => (minutes += x.minutes));
+
+    return `In the past 30 days, you've made a total of ${keys} keystrokes and coded ${minutes} minutes`;
+  };
+
+  const thisWeek = () => {
+    let thisWeek = new Date();
+    thisWeek.setDate(thisWeek.getDate() - 7);
+    let lastWeek = new Date();
+    lastWeek.setDate(lastWeek.getDate() - 14);
+    let keys = 0;
+    let minutes = 0;
+    projectData &&
+      allProjects(projectData)
+        .filter((x) => new Date(x.date) >= thisWeek)
+        .map((x) => (keys += x.keyStrokes));
+    projectData &&
+      allProjects(projectData)
+        .filter((x) => new Date(x.date) >= thisWeek)
+        .map((x) => (minutes += x.minutes));
+
+    let lastKeys = 0;
+    let lastMins = 0;
+    projectData &&
+      allProjects(projectData)
+        .filter(
+          (x) => new Date(x.date) >= lastWeek && new Date(x.date) < thisWeek
+        )
+        .map((x) => (lastKeys += x.keyStrokes));
+
+    projectData &&
+      allProjects(projectData)
+        .filter(
+          (x) => new Date(x.date) >= lastWeek && new Date(x.date) < thisWeek
+        )
+        .map((x) => (lastMins += x.minutes));
+
+    let keyDelta = (((keys - lastKeys) / lastKeys) * 100).toFixed(0);
+    let minDelta = (((minutes - lastMins) / lastMins) * 100).toFixed(0);
+
+    return keys >= lastKeys && minutes >= lastMins
+      ? `This week, your keystrokes were up ${keyDelta}% and your coding time was up ${minDelta}% compared to last week`
+      : keys >= lastKeys && minutes < lastMins
+      ? `This week, your keystrokes were up ${keyDelta}% and your coding time dropped ${minDelta}% compared to last week`
+      : keys < lastKeys && minutes >= lastMins
+      ? `This week, your keystrokes were down ${keyDelta}% and your coding time is up ${minDelta}% compared to last week`
+      : `This week, your keystrokes are down ${keyDelta}% and your coding time has dropped ${minDelta}% compared to last week`;
   };
 
   return (
@@ -143,7 +309,7 @@ const Data = (props) => {
         >
           <MenuItem aria-label="None" value="" />
           {projectData &&
-            projectData.map((x,index) => {
+            projectData.map((x, index) => {
               return (
                 <MenuItem key={index} value={x.name}>
                   {x.name}
@@ -152,60 +318,75 @@ const Data = (props) => {
             })}
         </Select>
       </FormControl>
-      {
-        state.selectedProject ? 
-          <FormControl variant="filled" className={classes.formControl}>
-            <InputLabel htmlFor="selectedProject-native-simple">File</InputLabel>
-            <Select
-              value={state.selectedFile}
-              name="selectedFile"
-              onChange={fileHandleChange}>
-              <MenuItem aria-label="None" value="" />
-              {projectData &&
-                projectData
-                  .filter((project) => project.name === state.selectedProject)
-                  .map((file) => {
-                    return file.fileData.map((x,index) => {
-                      return <MenuItem key={index} value={x.fileName}>{x.fileName}</MenuItem>;
-                    });
-                  })}
-            </Select>
-          </FormControl>
-        :null
-      }
-      
-      <Container className={classes.lineChart} maxWidth="lg">
-      {
-        state.data.length !== 0?
-        <ResponsiveContainer width="60%" height={300}>
-            <LineChart
-              data={state.selectedProject !== "" && state.selectedFile !== "" ? state.data : []}
-              margin={{
-                top: 5,
-                right: 30,
-                left: 20,
-                bottom: 5,
-            }}>
-            <CartesianGrid strokeDasharray="3 3" />
-            <XAxis dataKey="date" />
-            <YAxis />
-            <Tooltip />
-            <Legend />
-            <Line
-              type="monotone"
-              dataKey="keyStrokes"
-              stroke="#8884d8"
-              activeDot={{ r: 8 }}
-            />
-            <Line type="monotone" dataKey="minutes" stroke="#82ca9d" />
-          </LineChart>
-        </ResponsiveContainer>
-        :
-      null
-      }
-      </Container>
-      
-      
+      {state.selectedProject ? (
+        <FormControl variant="filled" className={classes.formControl}>
+          <InputLabel htmlFor="selectedProject-native-simple">File</InputLabel>
+          <Select
+            value={state.selectedFile}
+            name="selectedFile"
+            onChange={fileHandleChange}
+          >
+            <MenuItem aria-label="None" value="" />
+            {projectData &&
+              projectData
+                .filter((project) => project.name === state.selectedProject)
+                .map((file) => {
+                  return file.fileData.map((x, index) => {
+                    return (
+                      <MenuItem key={index} value={x.fileName}>
+                        {x.fileName}
+                      </MenuItem>
+                    );
+                  });
+                })}
+          </Select>
+        </FormControl>
+      ) : null}
+
+      <Grid container spacing={1}>
+        <Grid item xs={12} sm={6}>
+          {state.data.length !== 0 ? (
+            <ResponsiveContainer height={300}>
+              <LineChart
+                data={
+                  state.selectedProject !== "" && state.selectedFile !== ""
+                    ? state.data
+                    : []
+                }
+                margin={{
+                  top: 5,
+                  right: 30,
+                  left: 20,
+                  bottom: 5,
+                }}
+              >
+                <CartesianGrid strokeDasharray="3 3" />
+                <XAxis dataKey="date" />
+                <YAxis />
+                <Tooltip />
+                <Legend />
+                <Line
+                  type="monotone"
+                  dataKey="keyStrokes"
+                  stroke="#8884d8"
+                  activeDot={{ r: 8 }}
+                />
+                <Line type="monotone" dataKey="minutes" stroke="#82ca9d" />
+              </LineChart>
+            </ResponsiveContainer>
+          ) : null}
+        </Grid>
+        <Grid item xs={12} sm={6}>
+          <Paper className={classes.paper}>
+            <ul>
+              <li>{allTime()}</li>
+              <li>{thisWeek()}</li>
+              <li>{thirtyDays()}</li>
+              <li>{dayOfWeek()}</li>
+            </ul>
+          </Paper>
+        </Grid>
+      </Grid>
     </>
   );
 };
--- app/src/components/Home.jsx
+++ app/src/components/Home.jsx
@@ -2,7 +2,22 @@ import React from "react";
 import { withRouter } from "react-router-dom";
 import { makeStyles } from "@material-ui/core/styles";
 import colors from "../colors";
-import { useMediaQuery } from "@material-ui/core";
+import {
+  useMediaQuery,
+  FormControl,
+  InputLabel,
+  Select,
+  MenuItem,
+} from "@material-ui/core";
+import {
+  LineChart,
+  Line,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  Legend,
+} from "recharts";
 
 const { red, darkBlue, lightBlue } = colors;
 
@@ -11,13 +26,13 @@ const useStyles = makeStyles((theme) => ({
     margin: "20px",
     display: "flex",
     flexDirection: "row",
-    alignItems: "center"
+    alignItems: "center",
   },
   info: {
     display: "flex",
     flexDirection: "column",
     alignContent: "center",
-    margin: "10px"
+    margin: "10px",
   },
   button: {
     display: "flex",
@@ -30,32 +45,77 @@ const useStyles = makeStyles((theme) => ({
     outline: "none",
     "&:hover": {
       cursor: "pointer",
-      border: "1px solid" + darkBlue
+      border: "1px solid" + darkBlue,
     },
     "&:active": {
       backgroundColor: lightBlue,
-      border: "none"
-    }
+      border: "none",
+    },
   },
   words: {
     textAlign: "center",
-    margin: "8vh"
+    margin: "8vh",
   },
   graph: {
     margin: "10px",
     padding: "10px",
     height: "65vh",
-    width: "80vw"
-  }
+    width: "80vw",
+  },
 }));
 
 const mobileStyles = makeStyles((theme) => ({
   root: {
     display: "flex",
     flexDirection: "column",
-    alignItems: "center"
-  }
+    alignItems: "center",
+  },
 }));
+
+const data = [
+  {
+    name: "Page A",
+    uv: 4000,
+    pv: 2400,
+    amt: 2400,
+  },
+  {
+    name: "Page B",
+    uv: 3000,
+    pv: 1398,
+    amt: 2210,
+  },
+  {
+    name: "Page C",
+    uv: 2000,
+    pv: 9800,
+    amt: 2290,
+  },
+  {
+    name: "Page D",
+    uv: 2780,
+    pv: 3908,
+    amt: 2000,
+  },
+  {
+    name: "Page E",
+    uv: 1890,
+    pv: 4800,
+    amt: 2181,
+  },
+  {
+    name: "Page F",
+    uv: 2390,
+    pv: 3800,
+    amt: 2500,
+  },
+  {
+    name: "Page G",
+    uv: 3490,
+    pv: 4300,
+    amt: 2100,
+  },
+];
 
 const Home = () => {
   const clickHandler = (e) => {
@@ -72,7 +132,8 @@ const Home = () => {
   return (
     <div
       id="mainContent"
-      className={isTablet ? mobileClasses.root : classes.root}>
+      className={isTablet ? mobileClasses.root : classes.root}
+    >
       <div className={classes.info}>
         <p className={classes.words}>
           ProducDev is an extension built to track various metrics of your
@@ -81,14 +142,53 @@ const Home = () => {
         <button
           className={classes.button}
           onClick={clickHandler}
-          alt="click to get the extension">
+          alt="click to get the extension"
+        >
           Get the Extension
         </button>
       </div>
-      <img
-        className={classes.graph}
-        alt="graph"
-        src="https://i.pinimg.com/originals/1d/b2/fe/1db2fe7e19861900a2d9260cd1272727.jpg"></img>
+      {/* <FormControl variant="filled" className={classes.formControl}>
+        <InputLabel htmlFor="selectedProject-native-simple">Project</InputLabel>
+        <Select
+          value={state.selectedProject}
+          name="selectedProject"
+          onChange={projHandleChange}
+        >
+          <MenuItem aria-label="None" value="" />
+          {projectData &&
+            projectData.map((x, index) => {
+              return (
+                <MenuItem key={index} value={x.name}>
+                  {x.name}
+                </MenuItem>
+              );
+            })}
+        </Select>
+      </FormControl> */}
+      <LineChart
+        width={500}
+        height={300}
+        data={data}
+        margin={{
+          top: 5,
+          right: 30,
+          left: 20,
+          bottom: 5,
+        }}
+      >
+        <CartesianGrid strokeDasharray="3 3" />
+        <XAxis dataKey="name" />
+        <YAxis />
+        <Tooltip />
+        <Legend />
+        <Line
+          type="monotone"
+          dataKey="pv"
+          stroke="#8884d8"
+          activeDot={{ r: 8 }}
+        />
+        <Line type="monotone" dataKey="uv" stroke="#82ca9d" />
+      </LineChart>
     </div>
   );
 };
